8d3610b7eda684a61e5a95d5cc45e6f2532ad7fd
Dmytro Mykhailov <dmitry.pyrojoke.mikhaylov@gmail.com>
2016-12-16 11:57:20 +0200

    Initial SpecFlow sample

---
+++ SpecFlowPractice/GildedRose.feature
@@ -0,0 +1,28 @@
+﻿Feature: Gilded Rose
+	In order to track items state
+	As a shop owner
+	I want tool to calculate item quality with each passing day
+
+@unit
+Scenario: Item quality degrades and sell in date degrades with each passing date
+	Given I have the following items in stock
+	| Name    | Quality | Sell In |
+	| Item #1 | 10      | 4       |
+	| Item #2 | 7       | 2       |
+	When 1 day(s) pass
+	Then I have the following items in the stock
+	| Name    | Quality | Sell In |
+	| Item #1 | 9       | 3       |
+	| Item #2 | 6       | 1       |
+
+Scenario Outline: Processing items state in shop
+	Given I have item <Name> with quality <Quality> and Sell In days <Sell In>
+	When <Days> day(s) pass
+	Then I get quality <Result Quality> 
+
+	Examples: 
+	| Name                       | Quality | Sell In | Days | Result Quality |
+	| Item #1                    | 6       | 3       | 1    | 5              |
+	| Item #2                    | 7       | 5       | 2    | 5              |
+	| Item #3                    | 5       | 0       | 2    | 1              |
+	| Sulfuras, Hand of Ragnaros | 8       | 6       | 8    | 8              |
---
+++ SpecFlowPractice/GildedRoseSteps.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+using GR;
+
+namespace SpecFlowPractice
+{
+    [Binding]
+    public class GildedRoseSteps
+    {
+        private GildedRose _gildedRose;
+        private List<Item> _originalItems;
+
+        [Given(@"I have the following items in stock")]
+        public void GivenIHaveTheFollowingItemsInStock(List<Item> items)
+        {
+            _originalItems = items;
+            _gildedRose = new GildedRose(_originalItems);
+        }
+
+        [When(@"(.*) day\(s\) pass")]
+        public void WhenDayPass(int daysNumber)
+        {
+            for (int i = 0; i < daysNumber; i++)
+            {
+                _gildedRose.UpdateQuality();
+            }
+        }
+
+        [Then(@"I have the following items in the stock")]
+        public void ThenIHaveTheFollowingItemsInTheStock(List<Item> items)
+        {
+            items
+                .Zip(_originalItems, (expected, actual) => 
+                    expected.Name == actual.Name 
+                    && expected.Quality == actual.Quality 
+                    && expected.SellIn == actual.SellIn)
+                .All(t => t)
+                .Should()
+                .BeTrue();
+        }
+
+        private Item _currentItem;
+
+        [Given(@"I have item (.*) with quality (.*) and Sell In days (.*)")]
+        public void GivenIHaveItemItemWithQualityAndSellInDays(string name, int quality, int sellIn)
+        {
+            _currentItem = new Item { Name = name, Quality = quality, SellIn = sellIn};
+            _gildedRose = new GildedRose(new List<Item> { _currentItem});
+        }
+
+        [Then(@"I get quality (.*)")]
+        public void ThenIGetQuality(int resultQuality)
+        {
+            _currentItem.Quality.Should().Be(resultQuality);
+        }
+
+
+        [StepArgumentTransformation]
+        public List<Item> GetItemsFromTable(Table items)
+        {
+            return items.CreateSet<Item>().ToList();
+        }
+
+    }
+}
